e3b67c0ce2594ceb0750c493fde22e0fa4a68671
blydro klonk <blydro@gmail.com>
2018-05-09 12:14:39 +0200

    working cards!

--- views/food.js
+++ views/food.js
@@ -11,17 +11,23 @@ function view(state, emit) {
   function accept(place) {
     state.chosenPlace = place;
     // temp:
-    window.location = `https://citymapper.com/directions?startcoord=${state.position.latitude}%2C${state.position.longitude}&endcoord=${place.geometry.location.lat}%2C${place.geometry.location.lng}&endname=${place.name}&endaddress=${place.vicinity}`;
+    window.location = `https://citymapper.com/directions?startcoord=${
+      state.position.latitude
+    }%2C${state.position.longitude}&endcoord=${place.geometry.location.lat}%2C${
+      place.geometry.location.lng
+    }&endname=${place.name}&endaddress=${place.vicinity}`;
     console.log('celebrate!');
   }
 
-  const Place = function (place) {
+  const Place = function(place) {
     // TODO: handle 0 items found
     if (place === undefined) {
-      return state.currentPlaceIndex === undefined ? 'loading' : 'You didnt like anything. maybe try again?';
+      return state.currentPlaceIndex === undefined
+        ? 'loading'
+        : 'You didnt like anything. maybe try again?';
     }
-    return html`
-    <div>
+    const foodItem = html`
+    <div class="">
       ${place.name}
       <img class="w-30" src="${place.photo}" />
       rating: ${place.rating}
@@ -29,12 +35,41 @@ function view(state, emit) {
       <button onclick=${reject}>reject</button>
       <button onclick=${() => accept(place)}>accept</button>
     </div>`;
+    foodItem.addEventListener('touchstart', handleTouchStart);
+    foodItem.addEventListener('touchmove', handleTouchMove);
+    foodItem.addEventListener('touchend', handleTouchEnd);
+
+    let prevX = null;
+    let prevY = null;
+
+    function handleTouchStart(e) {
+      [prevX, prevY] = [
+        e.changedTouches[0].screenX,
+        e.changedTouches[0].screenY
+      ];
+    }
+    function handleTouchMove(e) {
+      const [x, y] = [e.changedTouches[0].screenX, e.changedTouches[0].screenY];
+      foodItem.style.transform = `translateX(${x - prevX}px)`;
+      console.log(x);
+    }
+    function handleTouchEnd(e) {
+      const [x, y] = [e.changedTouches[0].screenX, e.changedTouches[0].screenY];
+      if (x < window.innerWidth / 3) {
+        reject();
+      }
+      if (x > window.innerWidth - window.innerWidth / 3)
+        accept(state.places[state.currentPlaceIndex]);
+    }
+
+    return foodItem;
   };
   return html`
     <body class="helvetica">
-    <h1>${state.foods.find(food => food.name === state.params.wildcard).emoji}</h1>
+    <h1>${
+      state.foods.find(food => food.name === state.params.wildcard).emoji
+    }</h1>
     ${Place(state.places[state.currentPlaceIndex])}
     </body>
   `;
 }
-
