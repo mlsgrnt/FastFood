203e9af1ecca29054cda302081ce0ef94617b73e
blydro klonk <blydro@gmail.com>
2018-05-09 11:43:33 +0200

    fix no image

--- stores/maps.js
+++ stores/maps.js
@@ -5,13 +5,20 @@ module.exports = store;
 function store(state, emitter) {
   state.places = [];
   emitter.on('DOMContentLoaded', () => {
-    emitter.on('maps:findNearby', (food) => {
+    emitter.on('maps:findNearby', food => {
       state.places = []; // reset in case this is a second food
       // ""
-      fetch(`https://coral-biplane.glitch.me/nearby?location=${state.position.latitude},${state.position.longitude}&keyword=${food}&rankby=distance`, { mode: 'cors' }).then((data) => {
-      // fetch(`https://api.foursquare.com/v2/venues/search?ll=${state.position.latitude},${state.position.longitude}&intent=browse&radius=1500&query=${food}&client_id=${id}&client_secret=${secret}&v=20180507`, { mode: 'cors' }).then((data) => {
-        data.json().then((json) => {
-          state.places = json.results.filter(place => (place.opening_hours ? place.opening_hours.open_now : true));
+      fetch(
+        `https://coral-biplane.glitch.me/nearby?location=${
+          state.position.latitude
+        },${state.position.longitude}&keyword=${food}&rankby=distance`,
+        { mode: 'cors' }
+      ).then(data => {
+        // fetch(`https://api.foursquare.com/v2/venues/search?ll=${state.position.latitude},${state.position.longitude}&intent=browse&radius=1500&query=${food}&client_id=${id}&client_secret=${secret}&v=20180507`, { mode: 'cors' }).then((data) => {
+        data.json().then(json => {
+          state.places = json.results.filter(
+            place => (place.opening_hours ? place.opening_hours.open_now : true)
+          );
           state.currentPlaceIndex = 0;
           emitter.emit('maps:getDetails', 0, 2);
           emitter.emit(state.events.RENDER);
@@ -19,20 +26,29 @@ function store(state, emitter) {
       });
     });
 
-    emitter.on('maps:getDetail', (placeIndex) => {
+    emitter.on('maps:getDetail', placeIndex => {
       if (placeIndex > state.places.length - 1) {
         return; // we good
       }
       const endCoord = state.places[placeIndex].geometry.location;
-      fetch(`https://coral-biplane.glitch.me/citymapper?startcoord=${state.position.latitude},${state.position.longitude}&endcoord=${endCoord.lat},${endCoord.lng}`).then((data) => {
-        data.json().then((json) => {
+      fetch(
+        `https://coral-biplane.glitch.me/citymapper?startcoord=${
+          state.position.latitude
+        },${state.position.longitude}&endcoord=${endCoord.lat},${endCoord.lng}`
+      ).then(data => {
+        data.json().then(json => {
           state.places[placeIndex].travelTime = json.travel_time_minutes;
         });
       });
-      if (state.places[placeIndex].photos.length > 0) {
-        fetch(`https://coral-biplane.glitch.me/image?ref=${state.places[placeIndex].photos[0].photo_reference}`, { mode: 'cors' })
+      if (state.places[placeIndex].photos) {
+        fetch(
+          `https://coral-biplane.glitch.me/image?ref=${
+            state.places[placeIndex].photos[0].photo_reference
+          }`,
+          { mode: 'cors' }
+        )
           .then(response => response.blob())
-          .then((imageBlob) => {
+          .then(imageBlob => {
             state.places[placeIndex].photo = URL.createObjectURL(imageBlob);
             if (placeIndex === state.currentPlaceIndex) {
               emitter.emit(state.events.RENDER);
@@ -48,4 +64,3 @@ function store(state, emitter) {
     });
   });
 }
-
